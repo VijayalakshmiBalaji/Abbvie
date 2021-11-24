3c3e553a1072011fa2874f7f2ece99443abc8ecd
VijayalakshmiBalaji <93195522+VijayalakshmiBalaji@users.noreply.github.com>
2021-11-24 11:30:49 +0530

    Add files via upload

--- functions/utils/dtypes.py
+++ functions/utils/dtypes.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from typing import Any, Dict, List, Union
 
+# define data dict type
+DataDict = Dict[str, List[Any]]
+
+# input data type can be df or DataDict
+InputData = Union[pd.DataFrame, DataDict]
+
+# input data for comparison
+ComparisonData = Union[List[Any], Any]
